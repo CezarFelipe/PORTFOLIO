410528cb28ad61d950dcf7d5af9f645dee9dc437
CezarFelipe <cezarfelipedasilva@gmail.com>
2021-08-21 19:50:56 -0300

    Desenvolvimento da Aplicação:page error404

--- portfolio/src/router/index.js
+++ portfolio/src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Error404 from '../views/Error404.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '*',
+    name: 'Error404',
+    component: Error404
+  },
   {
     path: '/',
     name: 'Home',
