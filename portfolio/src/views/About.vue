0f64478a9d153b3514df78c8fb197c84618991a2
CezarFelipe <cezarfelipedasilva@gmail.com>
2021-08-26 21:06:21 -0300

    view

--- portfolio/src/views/About.vue
+++ portfolio/src/views/About.vue
@@ -1,8 +1,79 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <v-alert
-      type="success">
-    </v-alert>
+    <div class="about-body-top">
+      <div class="about-body-top-text">
+        <h1>About</h1>
+        <p>Paulistano, nascido na grande são paulo região do ABC, casado há mais de 9 anos, pai de um lindo casal de filhos.</p>
+        <p>Formado em Bacharel em Administração pela faculdade Uniesp, pós-graduado em Jogos Digitais pela faculdade Cruzeiro do Sul e pós-graduado em Engenharia de Software pela faculdade Estácio.</p>
+        <p>Apaixonado por programação, desenvolvedor fullstack, conheço tecnologias de front end como os framework Angular e VueJs, e Html, css e Java Script e no back end as linguagens Java e os framework quartz e spring, e C# e a engine Unity.</p>
+      </div>
+      <div class="about-body-top-image">
+      <img src="../assets/Rectangle 13.png" alt="">
+      </div>
+    </div>
+    <div class="about-body-bottom">
+      <div class="about-body-bottom-image">
+        <img src="../assets/Rectangle 2.png" alt="">
+      </div>
+      <div class="about-body-bottom-tex">
+      <p>Tenho um perfil alinhado com o conceito full cycle, que considero todos os pontos da aplicação, desde a concepção da ideia, arquitetura e deploy.</p>
+      <p>Também sou gamer designer, Art Designer e Sound Designer, e tenho como principla hobby jogar videogame.</p>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+</script>
+<style>
+h1
+{
+  font-size: 60px;
+  font-family: Uchen;
+  color:#5B3525;
+}
+p{
+  font-size: 18px;
+  font-family: Uchen;
+  color:#F7F7F7;
+  margin: 10px;
+}
+.about{
+width: 100%;
+height: 100%;
+background-color: #0D0D0D;
+}
+.about-body-top-text{
+width: 65%;
+height: 100%;
+float: left;
+}
+.about-body-top-image{
+width: 32%;
+height: 100%;
+float: left;
+}
+.about-body-top{
+width: 100%;
+height:65%;
+}
+.about-body-bottom{
+width: 100%;
+height: 35%;
+background-color:yellow;
+}
+.about-body-top-image img {
+  width: 100%;
+  height: 100%;
+}
+.about-body-bottom-image{
+  width: 15%;
+  height: 100%;
+  background-color: brown;
+  float: left;
+}
+.about-body-bottom-tex{
+  background-color: green;
+  height: 100%;
+  width: 100%;
+}
+</style>
