ebd9e99ef033f07d0ad143a5832c7d6a63ebe416
CezarFelipe <cezarfelipedasilva@gmail.com>
2021-09-11 16:41:39 -0300

    ajuste service

--- portfolio/src/service/api/api.js
+++ portfolio/src/service/api/api.js
@@ -1,10 +1,7 @@
-import axios from "axios";
-
-// Pode ser algum servidor executando localmente: 
-// http://localhost:3000
+import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
-});
+  baseURL: 'http://localhost:3000'
+})
 
-export default api;
+export default api
