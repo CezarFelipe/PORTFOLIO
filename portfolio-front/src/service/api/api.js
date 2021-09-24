1a1388857c802b36ef8913eb0323694fbe3f2b3f
CezarFelipe <cezarfelipedasilva@gmail.com>
2021-09-23 21:07:37 -0300

    ajuste host internal docker

--- portfolio-front/src/service/api/api.js
+++ portfolio-front/src/service/api/api.js
@@ -1,11 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://host.docker.internal::81/api'
+  baseURL: 'http://host.docker.internal:81/api'
 })
-
-/* const api = axios.create({
-  baseURL: 'http://localhost:3000'
-}) */
 
 export default api
